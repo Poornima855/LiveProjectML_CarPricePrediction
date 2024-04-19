7283450e6a15dfaf683e73a8fb9db6f841ab78b5
Poornima855 <poornimakatgi855@gmail.com>
2024-04-19 23:50:54 +0530

    App updated

--- setup.py
+++ setup.py
@@ -2,16 +2,15 @@ from setuptools import find_packages, setup
 from typing import List 
 
 HYPHEN_E_DOT = '-e .'
+# def get_requirements(file_path:str)-> List[str]: 
+#     requirements = []
+#     with open(file_path) as file_obj: 
+#         requirements = file_obj.readlines()
+#         requirements = [req.replace ("\n", "") for req in requirements]
 
-def get_requirements(file_path:str)-> List[str]: 
-    requirements = []
-    with open(file_path) as file_obj: 
-        requirements = file_obj.readlines()
-        requirements = [req.replace ("\n", "") for req in requirements]
-
-        if HYPHEN_E_DOT in requirements: 
-            requirements.remove(HYPHEN_E_DOT)
-    return requirements
+#         if HYPHEN_E_DOT in requirements: 
+#             requirements.remove(HYPHEN_E_DOT)
+#     return requirements
 
 setup(
     name = "MLPipeline", 
@@ -19,6 +18,6 @@ setup(
     author = "Poornima Katgi", 
     description= "An End to End ML project - Car Price",
     author_email= "poornimakatgi855@gmail.com", 
-    install_requires = get_requirements("requirements.txt"),
+#    install_requires = get_requirements("requirements.txt"),
     packages= find_packages()
     )
